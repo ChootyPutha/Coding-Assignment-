e9080f1ca4c76f1fb2fddc1f8fd91cbd33bb7e24
Dinesh Madushanka Senavirathna <dineshmadushankagss2015@gmail.com>
2025-04-28 16:44:18 +0530

    update legal screen ui changes

--- app/pages/NotificationScreen/NotificationScreen.tsx
+++ app/pages/NotificationScreen/NotificationScreen.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { View, StyleSheet, StatusBar, TextInput, Text, TouchableOpacity, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Ionicons } from '@expo/vector-icons';
 
 const isDarkMode = false;
 
@@ -18,13 +17,27 @@ const NoficationScreen = () => {
                     backgroundColor={backgroundStyle.backgroundColor}
                 />
                 <View style={styles.formHolder}>
-                    <View>
-                        <Text style={styles.infoText}>
-                            Get the most out of Blott ✅
-                        </Text>
-                        <Text style={styles.infoText}>
-                            Allow notifications to stay in the loop with your payments, requests and groups.
-                        </Text>
+                    <View style={styles.formContainer}>
+                        <View style={styles.cardItemContainer}>
+                            <Text style={styles.titelText}>
+                                Get the most out of Blott ✅
+                            </Text>
+                        </View>
+
+                        <View style={styles.cardItemContainer}>
+                            <Text style={styles.infoText}>
+                                Allow notifications to stay in the loop with your payments, requests and groups.
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <View style={styles.buttonHolder}>
+                        <TouchableOpacity style={styles.buttonwrapper}>
+                            <Text style={styles.buttonText}>
+                                Allow Notifications
+                            </Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
             </View>
@@ -41,19 +54,70 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    titelText: {
+        fontFamily: 'Roboto',
+        fontSize: 24,
+        fontWeight: '700',
+        color: '#000',
+        textAlign: 'center',
+    },
     infoText: {
         fontFamily: 'Roboto',
         fontSize: 16,
         fontWeight: '400',
         color: '#000',
+        textAlign: 'center',
     },
     formHolder: {
         width: '100%',
-        height : '40%',
+        height: '40%',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor:'green'
+        backgroundColor: 'green'
     },
+    formContainer: {
+        width: '90%',
+        height: '60%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    cardItemContainer : {
+        width: '100%',
+        height: '90%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding : 10
+    },
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 50,
+        width: '100%',
+        height: '20%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'pink'
+    },
+    buttonHolder: {
+        width: '90%',
+        height: '70%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonwrapper: {
+        width: '100%',
+        height: '60%',
+        borderRadius: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'blue'
+    },
+    buttonText: {
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        fontWeight: '500',
+        color: '#fff',
+    }
+
 
 
 });
